a0185aa514c2dddb2d5d38fd7a634aec18a02a5b
RudolfBun <Bun.Rudolf@stud.u-szeged.hu>
2020-12-02 23:21:39 +0100

    Hide sidenav on small screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -9,10 +9,27 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   public title = 'MyImdb';
   public isExpanded = false;
+  public isSmallScreen = false;
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) {
+    this.isSmallScreen = window.innerWidth < 750;
+  }
 
   public toggleExpanded(): void {
     this.isExpanded = !this.isExpanded;
+  }
+
+  public setToogleState(event: any) {
+    if (this.isExpanded && !event) {
+      this.isExpanded = !this.isExpanded;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event): void {
+    this.isSmallScreen = event.target.innerWidth < 750;
+    if (this.isSmallScreen && this.isExpanded) {
+      this.toggleExpanded();
+    }
   }
 }
